77f06ede3b8815c25b5b126023e0c92848790eb7
Nicholas Yoshida <nyoshida44@gmail.com>
2022-01-13 06:02:45 +0000

    Feature: Admin Categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,32 @@
+class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories]
+    else
+      render :new
+    end
+  end
+
+  private 
+  
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :category_id
+    )
+  end
+
+end
